7a0031e05bb6ff3febce7c27d84baba616c745ea
PoorMonk <heji_24@163.com>
2017-09-15 11:53:28 +0800

    add csFile of factory example

---
+++ Factory/Assets/Scripts/SpriteFactory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteFactory : MonoBehaviour {
+
+    private Object[] m_Objects;
+
+    private int m_index = 0;
+	// Use this for initialization
+	void Start () {
+        GetSprite("Number");
+	}
+
+    /// <summary>
+    /// 获取资源里的sprite图片
+    /// </summary>
+    public void GetSprite(string name)
+    {
+        m_Objects = Resources.LoadAll(name);
+        if(m_Objects == null)
+        {
+            Debug.Log("GetSprite failed...");
+        }
+        else
+        {
+            Debug.Log(m_Objects.Length);
+        }
+    }
+
+    public GameObject CreateSprite(int index)
+    {
+        GameObject tmpObj = new GameObject("Test");
+        Image tmpImg = tmpObj.AddComponent<Image>();
+        tmpImg.sprite = m_Objects[index] as Sprite;
+        return tmpObj;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            GameObject tmpObj = CreateSprite(m_index % 10 + 1);
+            m_index++;
+            tmpObj.transform.parent = transform;
+            tmpObj.transform.position = new Vector3(20 * m_index, 20, 0);
+        }
+	}
+}
---
+++ Factory/Assets/Scripts/TeacherFactory.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food
+{
+    public virtual void ShowMe()
+    {
+        Debug.Log("Food");
+    }
+}
+
+public class EggFood : Food
+{
+    public override void ShowMe()
+    {
+        Debug.Log("EggFood");
+    }
+}
+
+public class TomatoFood : Food
+{
+    public override void ShowMe()
+    {
+        //base.ShowMe();
+        Debug.Log("TomatoFood");
+
+    }
+}
+
+public class TeacherFactory : MonoBehaviour {
+
+    private Food m_food;
+    public string m_foodName;
+    // Use this for initialization
+    void Start () {
+        m_food = CreateFood(m_foodName);
+        if(m_food != null)
+        {
+            m_food.ShowMe();
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Food CreateFood(string strFoodName)
+    {
+        if("egg" == strFoodName.ToLower())
+        {
+            return new EggFood();
+        }
+        else if("tomato" == strFoodName.ToLower())
+        {
+            return new TomatoFood();
+        }
+        else
+        {
+            Debug.Log("Don't have this food...");
+            return null;
+        }     
+    }
+}
